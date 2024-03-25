e12da09fb75176b1ec98ec23db615d058385e9de
Limonek <limonek@kozak.iphole>
2024-03-25 14:13:00 +0100

    initial commit

---
+++ app/src/main/java/com/pixelframe/model/ImageConverter.java
@@ -0,0 +1,15 @@
+package com.pixelframe.model;
+
+/**
+ * Container for methods helping to convert bitmap from a bitmap to
+ * format specific for PicoFrame .pfb
+ */
+public class ImageConverter {
+    private int width, height;
+    ImageConverter(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    voi
+}
