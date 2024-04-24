203b0f89acce30bf5f39f2ff639d2f8f2459de95
heracro <pawel.mielimonka@proton.me>
2024-04-24 17:05:44 +0200

    load image now opens galery (but images are too large)

--- app/src/main/java/com/pixelframe/controller/ui/MainActivity.java
+++ app/src/main/java/com/pixelframe/controller/ui/MainActivity.java
@@ -1,26 +1,45 @@
 package com.pixelframe.controller.ui;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import com.pixelframe.controller.R;
+import com.github.chrisbanes.photoview.PhotoView;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityResultLauncher<String> mGetContent;
+    private PhotoView photoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        photoView = findViewById(R.id.photo_view);
+        Button buttonLoadImage = findViewById(R.id.button_load);
+
+        mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+                uri -> {
+                    if (uri != null) {
+                        Picasso.get().load(uri).into(photoView);
+                    }
+                }
+        );
+
+        buttonLoadImage.setOnClickListener(v -> {
+            mGetContent.launch("image/*");
         });
     }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == REQUEST_CODE_STORAGE_PERMISSION && resultCode == RESULT_OK && data != null) {
+//            Uri imageUri = data.getData();
+//            PhotoView photoView = findViewById(R.id.photo_view);
+//            Picasso.get().load(imageUri).into(photoView);
+//        }
+//    }
 }
