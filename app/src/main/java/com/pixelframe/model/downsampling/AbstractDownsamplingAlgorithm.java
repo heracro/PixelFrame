fcde9a6a42f568d971d5afc70fde04120c49f931
heracro <pawel.mielimonka@proton.me>
2024-09-09 16:12:43 +0200

    Little bit less logging from what we know already works just fine.

--- app/src/main/java/com/pixelframe/model/downsampling/AbstractDownsamplingAlgorithm.java
+++ app/src/main/java/com/pixelframe/model/downsampling/AbstractDownsamplingAlgorithm.java
@@ -26,28 +26,19 @@ public abstract class AbstractDownsamplingAlgorithm {
         int columnStart = 0;
         int rowStart = 0;
         float exactFragmentWidth = (float)width / columns;
-        Log.d("Abstract Downsampling Algorithm","exactFragmentWidth = " + exactFragmentWidth);
         float exactFragmentHeight = (float)height / rows;
-        Log.d("Abstract Downsampling Algorithm","exactFragmentHeight = " + exactFragmentHeight);
         for (int c = 0; c < columns; ++c) {
             int columnEnd = Math.round(exactFragmentWidth * (c + 1));
-            Log.d("Abstract Downsampling Algorithm","columnEnd = " + columnEnd);
             if (columnEnd >= width) {
-                Log.d("Abstract Downsampling Algorithm", "convert(): columnEnd = " + columnEnd + "; width = "+ width);
                 columnEnd = width -1;
             }
             for (int r = 0; r < rows; ++r) {
                 int rowEnd = Math.round(exactFragmentHeight * (r + 1));
-                Log.d("Abstract Downsampling Algorithm","rowEnd = " + rowEnd);
                 if (rowEnd >= height) {
-                    Log.d("Abstract Downsampling Algorithm", "convert(): rowEnd = " + rowEnd + "; height = "+ height);
                     rowEnd = height -1;
                 }
-                Log.d("Abstract Downsampling Algorithm", "Trying to call: Bitmap.createBitmap(image, "
-                    + columnStart + ", " + rowStart + ", " + (columnEnd - columnStart) + ", " + (rowEnd - rowStart) + ");");
                 Bitmap fragment = Bitmap.createBitmap(image, columnStart, rowStart,
                         columnEnd - columnStart, rowEnd - rowStart);
-                Log.d("Abstract Downsampling Algorithm", "Bitmap created. Attempting to convert fragment into pixel");
                 int color = convertFragment(fragment);
                 result.setPixel(c, r, color);
                 rowStart = rowEnd;
